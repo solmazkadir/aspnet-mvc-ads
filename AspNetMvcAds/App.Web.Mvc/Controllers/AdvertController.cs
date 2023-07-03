ae26099bc5239496d82f1c220e5bdb6118c74cf8
solmazkadir <abdlkadirsolmaz@gmail.com>
2023-07-03 20:23:17 +0300

    kontroller oluşturuldu

---
+++ AspNetMvcAds/App.Web.Mvc/Controllers/AdvertController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class AdvertController : Controller
+    {
+        [Route("/advert/title-slug")]
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ AspNetMvcAds/App.Web.Mvc/Controllers/PageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class PageController : Controller
+    {
+        //[Route("/page/title-slug")]
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
