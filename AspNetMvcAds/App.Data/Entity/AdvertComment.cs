1bb3bb671963c8d09e0faccf15d65f35020816e7
Behruzsv <126698226+Behruzsv@users.noreply.github.com>
2023-07-09 23:19:44 +0300

    Advert-Comment

--- AspNetMvcAds/App.Data/Entity/AdvertComment.cs
+++ AspNetMvcAds/App.Data/Entity/AdvertComment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace App.Data.Entity
 {
     internal class AdvertComment
     {
+        public int Id { get; set; }
+        [Display(Name = "İlanId")]
+        public int AdvertId { get; set; }
+        [Display(Name = "KullanıcıId")]
+        public int UserId { get; set; }
+        [Display(Name = "Yorum")]
+        public string Comment { get; set; }
+        [Display(Name = "Durum")]
+        public bool IsActive { get; set; }
     }
 }
