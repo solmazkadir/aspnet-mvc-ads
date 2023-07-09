e86098183e0e1158ef8d1d55c053a61eeeddf250
solmazkadir <abdlkadirsolmaz@gmail.com>
2023-07-09 22:51:42 +0300

    Ekleme

--- AspNetMvcAds/App.Data/Entity/Setting.cs
+++ AspNetMvcAds/App.Data/Entity/Setting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,10 @@ namespace App.Data.Entity
     public class Setting : IEntity
     {
         public int Id { get; set; }
+        [Display(Name = "Kullanıcı Id")]
+        public User UserId { get; set; }
+        [Display(Name = "İsim")]
+        public string Name { get; set; }
+        public string Value { get; set; }
     }
 }
